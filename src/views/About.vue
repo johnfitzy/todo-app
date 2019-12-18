892d97b1b4d6430d63423490db2002abe9cf7378
John Fitzgerald <johnf@datacom.co.nz>
2019-12-18 15:29:18 +1300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>View crash course on youtube: https://www.youtube.com/watch?v=Wy9q22isx3U&t=2068s</p>
+    <p>View crash course on youtube... https://www.youtube.com/watch?v=Wy9q22isx3U&t=2068s</p>
   </div>
 </template>
